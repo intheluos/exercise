d6e11400a4cdbe8a186d57f5f4870f8e51cb64cc
lizzy <lizzy@playpurplecat.com>
2014-02-28 13:55:44 +0900

    씬 바꾸기 버튼

---
+++ my_room1/Assets/SceneButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnGUI(){
+		if (GUI.Button(new Rect(10,10,100,40), "Change")){
+		
+			Application.LoadLevel("3");
+	}
+	
+}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ my_room1/Assets/SceneButton1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneButton1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnGUI(){
+		if (GUI.Button(new Rect(10,10,100,40), "Return")){
+			
+			Application.LoadLevel("1");
+		
+		}
+	}		
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
