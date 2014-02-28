51f9553ce16c471d537fdb9809482900e16abdc1
lizzy <lizzy@playpurplecat.com>
2014-02-28 18:56:15 +0900

    no message

---
+++ my_room1/Assets/Resource/Script/ButtonManeger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManeger : MonoBehaviour
+{
+		public Texture TexNormal;
+		public Texture TexDown;
+		
+		void OnMouseEnter()
+	{
+	guiTexture.color = new Color (0.8f, 0.8f, 0.8f, 1);	
+	}	
+		void OnMouseExit()
+	{
+	guiTexture.color = new Color (0.5f, 0.5f, 0.5f, 1);	
+	}
+	
+	void OnMouseDown()
+	{
+		guiTexture.texture = TexDown;
+	}
+	
+	void OnMouseUp()
+	{
+		guiTexture.texture = TexNormal;
+	}
+}
